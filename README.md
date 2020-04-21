5ccbd00cf393d644c81c9f0764ad760e620c1d1d
bugsanderrors <open.nishant@gmail.com>
2020-04-21 21:08:24 +0530

    Add Week 1 Question 3

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Solutions to 30 day LeetCode Challenge (Solution will be uploaded after the deadline for the problem is ended. No cheating.)
 
 Solution for a particular problem will be in C++ or Python. :octocat:
+(Only function definition is there!) :camel:
 
 I will try to provide both C++ and Python solution for each problem when the Challenge is over. :rocket:
 
---
+++ W1Q3.cpp
@@ -0,0 +1,28 @@
+/*
+    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+    Example:
+
+    Input: [-2,1,-3,4,-1,2,1,-5,4],
+    Output: 6
+    Explanation: [4,-1,2,1] has the largest sum = 6.
+
+    Follow up:
+
+    If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+
+
+int maxSubArray(vector<int>& nums) 
+{       
+    int max_so_far = nums[0]; 
+    int curr_max = nums[0]; 
+
+    for (int i = 1; i < nums.size(); i++) 
+    { 
+        curr_max = max(nums[i], curr_max+nums[i]); 
+        max_so_far = max(max_so_far, curr_max); 
+    }
+
+    return max_so_far; 
+}
